2bee9b6ac5b0f609a864744aab9ca40c2be4303e
Jhonatan <jhonatan.saman@ufsc.br>
2019-09-12 15:55:45 -0300

    all

--- src/screens/passo2/grade.js
+++ src/screens/passo2/grade.js
@@ -19,6 +19,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import { Checkbox } from 'semantic-ui-react'
 
 //COMPONENTES
 import Header from '../../components/header';
@@ -30,25 +31,25 @@ import { Link } from 'react-router-dom'
 const drawerWidth = "25%";
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        boxShadow: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-    },
-    toolbar: theme.mixins.toolbar,
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    boxShadow: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
 })
 
 class Grade extends React.Component {
@@ -58,7 +59,7 @@ class Grade extends React.Component {
     this.state = {
       selectedOption: null,
       id_curso: '',
-      array: [{value: '', label: ''}],
+      array: [{ value: '', label: '' }],
       carregou: '',
       carregouDisciplina: '',
       carregouTurma: '',
@@ -70,32 +71,32 @@ class Grade extends React.Component {
 
   handleChangeSemestre = async selectedOptionSemestre => {
     await this.setState({ selectedOptionSemestre });
-    
+
     const response = await api.post("/disciplina/obter", {
       codigo_curso: this.state.id_curso, fase: this.state.selectedOptionSemestre.value
     })
 
-    this.setState({disciplinas: '', carregouDisciplina: false, turmas: '', carregouTurma: false});
+    this.setState({ disciplinas: '', carregouDisciplina: false, turmas: '', carregouTurma: false });
 
     for (var i = 0; i < response.data.length; i++) {
       var nome = response.data[i].codigo;
       nome += ' - ';
       nome += response.data[i].nome;
-      await this.setState({disciplinas: [...this.state.disciplinas, {nome: nome, id_disciplina: response.data[i].id_disciplina}], carregouDisciplina: true})
+      await this.setState({ disciplinas: [...this.state.disciplinas, { nome: nome, id_disciplina: response.data[i].id_disciplina }], carregouDisciplina: true })
     }
 
   };
 
   async handleChangeDisciplina(item, index) {
-    
+
     const response = await api.post("/disciplina/buscarTurmas", {
       id_disciplina: item.id_disciplina
     })
 
-    this.setState({turmas: '', carregouTurma: false});
+    this.setState({ turmas: '', carregouTurma: false });
 
     for (var i = 0; i < response.data.length; i++) {
-      await this.setState({turmas: [...this.state.turmas, {id_turma: response.data[i].id_plano_ensino, turma: response.data[i].turma}], carregouTurma: true})
+      await this.setState({ turmas: [...this.state.turmas, { id_turma: response.data[i].id_plano_ensino, turma: response.data[i].turma }], carregouTurma: true })
     }
 
     console.log("Index: ", index);
@@ -103,106 +104,110 @@ class Grade extends React.Component {
   };
 
   async handleChangeTurma(item) {
-    
-    await this.setState({carregouGerar: true})
+
+    await this.setState({ carregouGerar: true })
 
   };
 
   async handleChangeComponente(item) {
-    
-    await this.setState({carregouComponente: true})
+
+    await this.setState({ carregouComponente: true })
 
   };
 
   componentDidMount = async () => {
     if (localStorage.getItem('login') == 'on') {
       var usuario = JSON.parse(localStorage.getItem('usuario'))
- 
+
       this.setState({ id_curso: usuario.id_curso });
- 
+
       const response = await api.post("/disciplina/buscarSemestre", {
         id_course: usuario.id_curso
       })
- 
+
       await this.setState({ qtdeSemestre: response.data[0].semestres, })
       var rows = [];
       for (var i = 1; i <= this.state.qtdeSemestre; i++) {
-        this.setState({array: [...this.state.array, {value: i, label: i}] })
+        this.setState({ array: [...this.state.array, { value: i, label: i }] })
         this.setState({ carregou: true })
       }
- 
+
     }
 
   };
-  
-  render(){
+
+  render() {
 
     const { selectedOptionSemestre } = this.state;
     const self = this;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Header />
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="right"
-      >  
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Header />
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="right"
+        >
 
-        <div style={{marginTop: 150 + 'px'}}></div>
+          <div style={{ marginTop: 150 + 'px' }}></div>
 
-        { this.state.carregouComponente ? 
-          <div>{this.state.disciplinas.nome + '' + this.state.turmas.nome}</div>
-          : null
-        }
+          {this.state.carregouComponente ?
+            <div>{this.state.disciplinas.nome + '' + this.state.turmas.nome}</div>
+            : null
+          }
 
-        <Select id="cadastro_turmas_input_1"
-          value={selectedOptionSemestre}
-          onChange={this.handleChangeSemestre}
-          options={this.state.array}
-        />
+          <Select id="cadastro_turmas_input_1"
+            value={selectedOptionSemestre}
+            onChange={this.handleChangeSemestre}
+            options={this.state.array}
+          />
 
 
-        { this.state.carregouDisciplina ? 
-          this.state.disciplinas.map(function(item, index) {
-            return(
-            <div> <input onClick={() => self.handleChangeDisciplina(item, index)} name="input" value={index} type="radio"/>
-            <p >{item.nome}</p></div>
-           )
-          })
-          : null
-        }
+          {this.state.carregouDisciplina ?
+            this.state.disciplinas.map(function (item, index) {
+              return (
+                <div
+                  style={{ flexDirection: 'row', display:'flex', marginTop: 10 }}
+                >
+                  <input style={{marginRight: 10}} onClick={() => self.handleChangeDisciplina(item, index)} name="input" value={index} type="radio" />
+                  <p>{item.nome}</p>
+                </div>
+              )
+            })
+            : null
+          }
 
-        { this.state.carregouTurma ? 
-          this.state.turmas.map(function(item) {
-            return(
-            <div> <input onClick={() => self.handleChangeTurma(item)} type="checkbox"/>
-            <p >{item.turma}</p></div>
-           )
-          })
-          : null
-        }
+          {this.state.carregouTurma ?
+            this.state.turmas.map(function (item) {
+              return (
+                <div> <input onClick={() => self.handleChangeTurma(item)} type="checkbox" />
+                  <p >{item.turma}</p></div>
+              )
+            })
+            : null
+          }
 
-        { this.state.carregouGerar ? 
-          <div><button onClick={() => self.handleChangeComponente()}>Gerar Componente</button></div>
-          : null
-        }
-        
+          {this.state.carregouGerar ?
+            <div><button onClick={() => self.handleChangeComponente()}>Gerar Componente</button></div>
+            : null
+          }
 
-        
 
-      </Drawer>
 
-      <main className={classes.content}>
-      </main>
 
-    </div>
+        </Drawer>
+
+        <main className={classes.content}>
+        </main>
+
+      </div>
     );
   }
 }
