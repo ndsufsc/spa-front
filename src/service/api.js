159de84a0129b34637fa5e6537a977cade5f673c
Gzuis <filipi.garcia7@gmail.com>
2019-09-18 14:13:12 -0300

    commit legal

--- src/service/api.js
+++ src/service/api.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: 'https://backend-spa-teste-local.herokuapp.com',
-    baseURL: 'https://backend-spa-teste-local.herokuapp.com/',
-=======
     baseURL: 'https://backend-spa.herokuapp.com/',
->>>>>>> ef9db4f5bd0c3e045998b06b092084c03dd5ca8d
 })
 
 export default api;
